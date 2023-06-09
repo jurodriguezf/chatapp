e7af10774e00253d078ecfd8251255be9ab4285e
jurodriguezf <jurodriguezf@unal.edu.co>
2023-06-08 19:55:14 -0500

    Captcha validation and email login implementation

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useRef, useState } from 'react';
+import React, { useRef, useState, useEffect } from 'react';
 import './App.css';
 
 import firebase from 'firebase/compat/app';
@@ -7,6 +7,8 @@ import 'firebase/compat/auth';
 
 import { useAuthState } from 'react-firebase-hooks/auth';
 import { useCollectionData } from 'react-firebase-hooks/firestore';
+
+import HCaptcha from '@hcaptcha/react-hcaptcha';
 
 firebase.initializeApp ({
   apiKey: "AIzaSyD_-nra3CC3Wf35wJYqVg3G11S-q1OTuN4",
@@ -39,14 +41,87 @@ function App() {
 }
 
 function SignIn() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const [captchaToken, setCaptchaToken] = useState('');
+  const captchaRef = useRef(null);
+
+  const onLoad = () => {
+    captchaRef.current.execute();
+  };
+
+  useEffect(() => {
+    if (captchaToken)
+      console.log(`hCaptcha Token: ${captchaToken}`);
+  }, [captchaToken]);
+
   const signInWithGoogle = () => {
     const provider = new firebase.auth.GoogleAuthProvider();
     auth.signInWithPopup(provider);
   }
 
-  return (
-    <button className='sign-in' onClick={signInWithGoogle}>Sign in with Google</button>
-  )
+  const signInWithEmailAndPassword = async (e) => {
+    e.preventDefault();
+
+    if (!captchaToken) {
+      console.log('Please complete the CAPTCHA validation.');
+      return;
+    }
+
+    try {
+      await auth.signInWithEmailAndPassword(email, password);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  // Unused for now
+  const signUpWithEmailAndPassword = async (e) => {
+    e.preventDefault();
+
+    try {
+      await auth.createUserWithEmailAndPassword(email, password);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (<>
+    <div>
+      <div className='captcha-text'>
+        Please complete the Captcha before email login
+      </div>
+      <HCaptcha
+        sitekey="0cd94277-a276-458f-9765-9ada00843da3"
+        onLoad={onLoad}
+        onVerify={setCaptchaToken}
+        ref={captchaRef}
+      />
+      <form className="email-sign-in-form" onSubmit={signInWithEmailAndPassword}>
+        <input
+          type="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          placeholder="Email"
+        />
+        <input
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          placeholder="Password"
+        />
+        
+        <button type="submit">Sign in</button>
+      </form>
+
+      <hr/>
+
+      <button className="sign-in-google" onClick={signInWithGoogle}>
+        Sign in with Google
+      </button>
+    </div>
+  </>)
 }
 
 function SignOut() {
@@ -89,7 +164,7 @@ function ChatRoom() {
       <div ref={dummy}></div>
     </main>
 
-    <form onSubmit={sendMessage}>
+    <form className='send-message-form' onSubmit={sendMessage}>
       <input value={formValue} onChange={(e) => setFormValue(e.target.value)} placeholder='Write a message'/>
       <button className='sendButton' type='submit'>Send</button>
     </form>
@@ -103,7 +178,7 @@ function ChatMessage(props) {
 
   return (<>
     <div className={`message ${messageClass}`}>
-      <img src={photoURL || 'https://st3.depositphotos.com/6672868/13701/v/450/depositphotos_137014128-stock-illustration-user-profile-icon.jpg'}></img>
+      <img alt='User profile picture' src={photoURL || 'https://st3.depositphotos.com/6672868/13701/v/450/depositphotos_137014128-stock-illustration-user-profile-icon.jpg'}></img>
       <p>{text}</p>
     </div>
   </>)
